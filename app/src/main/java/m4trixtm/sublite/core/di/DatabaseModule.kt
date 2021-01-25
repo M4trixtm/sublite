757e994ec9472fa42d7b4baeaa4eecedb71c6f68
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-25 12:28:44 +0330

    Add database DI module

---
+++ app/src/main/java/m4trixtm/sublite/core/di/DatabaseModule.kt
@@ -0,0 +1,21 @@
+package m4trixtm.sublite.core.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import m4trixtm.sublite.core.database.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase =
+        Room.databaseBuilder(context, AppDatabase::class.java, "db").build()
+}
