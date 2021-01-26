a5e0b00c78c84b6db887cdb17a38f52eee6e4596
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-26 19:53:41 +0330

    Create base ApiResponse

---
+++ app/src/main/java/m4trixtm/sublite/features/ApiResponse.kt
@@ -0,0 +1,23 @@
+package m4trixtm.sublite.features
+
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponse<T>(
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_count")
+    val totalCount: Int,
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("data")
+    val data: List<Data<T>>
+)
+
+data class Data<T>(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("type")
+    val type: String,
+    @SerializedName("attributes")
+    val attributes: T
+)
