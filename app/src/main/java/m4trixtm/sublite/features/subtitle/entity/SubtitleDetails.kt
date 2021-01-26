0c7fe8532c28ab8e76709b2f3b8c33314a6861e9
moein <Moeinabdi77@gmail.com>
2021-01-26 21:05:38 +0330

    Inherit entity classes from BaseEntity

--- app/src/main/java/m4trixtm/sublite/features/subtitle/entity/SubtitleDetails.kt
+++ app/src/main/java/m4trixtm/sublite/features/subtitle/entity/SubtitleDetails.kt
@@ -2,11 +2,12 @@ package m4trixtm.sublite.features.subtitle.entity
 
 import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
+import m4trixtm.sublite.core.platform.entity.BaseEntity
 
 @Keep
 data class SubtitleDetails(
     @SerializedName("feature_id")
-    val id: Long,
+    override val id: Long,
     @SerializedName("title")
     val title: String,
     @SerializedName("movie_name")
@@ -19,4 +20,4 @@ data class SubtitleDetails(
     val imdbId: Int,
     @SerializedName("tmdb_id")
     val tmdbId: Int
-)
+): BaseEntity<Long>
