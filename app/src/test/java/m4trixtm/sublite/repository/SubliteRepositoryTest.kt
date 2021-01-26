b93f5d83a12e1e1a016aad333a5268f24667935a
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-26 19:57:13 +0330

    Create SubtitleRepository Test SubtitleRepository

---
+++ app/src/test/java/m4trixtm/sublite/repository/SubliteRepositoryTest.kt
@@ -0,0 +1,60 @@
+package m4trixtm.sublite.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import m4trixtm.sublite.MainCoroutinesRule
+import m4trixtm.sublite.SubtitleMocks
+import m4trixtm.sublite.features.ApiResponse
+import m4trixtm.sublite.features.subtitle.SubtitleRepositoryImpl
+import m4trixtm.sublite.features.subtitle.SubtitleService
+import m4trixtm.sublite.features.subtitle.entity.Subtitle
+import m4trixtm.sublite.features.subtitle.entity.SubtitleDownloadLink
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SubliteRepositoryTest {
+
+    private val service: SubtitleService = mock()
+    private lateinit var repository: SubtitleRepositoryImpl
+
+    @get:Rule
+    @ExperimentalCoroutinesApi
+    var coroutinesRule = MainCoroutinesRule()
+
+    @get:Rule
+    var instantExecutor = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        repository = SubtitleRepositoryImpl(service)
+    }
+
+    @Test
+    fun `Search subtitles`() = runBlocking {
+        val searchQuery = "interstellar"
+        val apiResponse: ApiResponse<Subtitle> = SubtitleMocks.searchResponse
+
+        whenever(service.search(searchQuery)).thenReturn(apiResponse)
+        repository.search(searchQuery, {}, {}).first()
+        verify(service).search(searchQuery)
+        Unit
+    }
+
+    @Test
+    fun `Get download link`() = runBlocking {
+        val subtitleId = 12345
+        val apiResponse: SubtitleDownloadLink = SubtitleMocks.downloadLinkResponse
+
+        whenever(service.getDownloadLink(subtitleId)).thenReturn(apiResponse)
+        repository.getDownloadLink(subtitleId, {}, {}).first()
+        verify(service).getDownloadLink(subtitleId)
+
+        Unit
+    }
+}
