49ee196eca43cf7a09f9809a3573a5ceb4002cec
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-27 12:56:25 +0330

    Change BaseRepository networkRequest function to be compatible with NetworkResponse

--- app/src/main/java/m4trixtm/sublite/core/platform/repository/BaseRepository.kt
+++ app/src/main/java/m4trixtm/sublite/core/platform/repository/BaseRepository.kt
@@ -1,26 +1,26 @@
 package m4trixtm.sublite.core.platform.repository
 
+import com.haroldadmin.cnradapter.NetworkResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import m4trixtm.sublite.core.log.appLog
-import java.io.IOException
+import m4trixtm.sublite.core.platform.model.NetworkError
 
 abstract class BaseRepository {
 
-    fun <T> networkRequest(
-        request: suspend () -> T,
+    fun <T : Any> networkRequest(
+        request: suspend () -> NetworkResponse<T, NetworkError>,
         onSuccess: () -> Unit,
         onError: (message: String) -> Unit
     ): Flow<T> = flow {
-        try {
-            val response = request()
-            onSuccess()
-            emit(response)
-        } catch (e: IOException) {
-            e.message?.let {
-                onError(it)
-                appLog(message = { it })
+
+        when (val response = request()) {
+            is NetworkResponse.Success -> {
+                onSuccess()
+                emit(response.body)
             }
+            is NetworkResponse.NetworkError -> onError("Network Error")
+            is NetworkResponse.ServerError -> onError("Server Error")
+            is NetworkResponse.UnknownError -> onError("Unknown Error")
         }
     }
 }
