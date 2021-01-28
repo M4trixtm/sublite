d38573f81d563a51df9c269409754fdff169fb1f
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-28 12:43:47 +0330

    Update platform

--- app/src/main/java/m4trixtm/sublite/core/platform/fragment/BaseFragment.kt
+++ app/src/main/java/m4trixtm/sublite/core/platform/fragment/BaseFragment.kt
@@ -1,12 +1,26 @@
 package m4trixtm.sublite.core.platform.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<T : ViewBinding>(@LayoutRes private val layoutRes: Int) : Fragment(layoutRes) {
+abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    Fragment(layoutRes) {
 
-    fun <T : ViewBinding> BaseFragment<T>.viewBinding(viewBindingFactory: (View) -> T) =
-        FragmentViewBindingDelegate(this, viewBindingFactory)
+    lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, layoutRes, container, false)
+        return binding.root
+    }
 }
