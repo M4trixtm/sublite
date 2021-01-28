c4e61e1c86cdcd5e784f5325eafbdf2906e5940c
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-28 10:14:48 +0330

    Update dagger-hilt to 2.31

--- app/src/main/java/m4trixtm/sublite/core/di/NetworkModule.kt
+++ app/src/main/java/m4trixtm/sublite/core/di/NetworkModule.kt
@@ -4,14 +4,14 @@ import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     const val BASE_URL = "https://www.opensubtitles.com/api/v1/"
