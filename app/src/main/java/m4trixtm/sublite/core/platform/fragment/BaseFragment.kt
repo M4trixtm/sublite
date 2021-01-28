d719796bcde72f225621d905bbf6983da05aae4c
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-28 21:51:23 +0330

    Clean up Import java 11

--- app/src/main/java/m4trixtm/sublite/core/platform/fragment/BaseFragment.kt
+++ app/src/main/java/m4trixtm/sublite/core/platform/fragment/BaseFragment.kt
@@ -8,7 +8,6 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
     Fragment(layoutRes) {
