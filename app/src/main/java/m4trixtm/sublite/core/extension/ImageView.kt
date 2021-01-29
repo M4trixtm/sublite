67205c048d97d453e3339365ee839ec4b607b19b
yas <mryas3352@gmail.com>
2021-01-29 20:12:48 +0330

    Add movie poster to subtitle search item. Closes #14

--- app/src/main/java/m4trixtm/sublite/core/extension/ImageView.kt
+++ app/src/main/java/m4trixtm/sublite/core/extension/ImageView.kt
@@ -4,12 +4,13 @@ import android.graphics.Bitmap
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.Transformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
-fun ImageView.loadFromUrl(url: String, vararg transformations: Transformation<Bitmap> = arrayOf()) =
+fun ImageView.loadFromUrl(url: String) =
     Glide.with(context)
         .load(url)
-        .transform(*transformations)
-        .centerCrop()
+        .transform(CenterCrop(), RoundedCorners(16))
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(this)
