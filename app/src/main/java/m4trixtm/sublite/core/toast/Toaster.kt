126ff3be549351247d9638e3aefabaed3d158779
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-29 13:32:26 +0330

    Update Toaster extensions

--- app/src/main/java/m4trixtm/sublite/core/toast/Toaster.kt
+++ app/src/main/java/m4trixtm/sublite/core/toast/Toaster.kt
@@ -2,11 +2,16 @@ package m4trixtm.sublite.core.toast
 
 import android.content.Context
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 
-infix fun Context.shortToast(message: () -> String) {
-    Toast.makeText(this, message(), Toast.LENGTH_SHORT).show()
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-infix fun Context.longToast(message: () -> String) {
-    Toast.makeText(this, message(), Toast.LENGTH_LONG).show()
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
+
+fun Fragment.shortToast(message: String) = requireContext().shortToast(message)
+
+fun Fragment.longToast(message: String) = requireContext().longToast(message)
