65282b1cd8abc84c84d9fe62e50d4995ed44c4af
moein <Moeinabdi77@gmail.com>
2021-01-29 22:12:14 +0330

    update SubtitleService.kt to add getMostDownloaded and getLatestSubtitles

--- app/src/main/java/m4trixtm/sublite/features/subtitle/SubtitleService.kt
+++ app/src/main/java/m4trixtm/sublite/features/subtitle/SubtitleService.kt
@@ -15,4 +15,24 @@ interface SubtitleService {
     @FormUrlEncoded
     @POST("download")
     suspend fun getDownloadLink(@Field("file_id") fileId: Int): NetworkResponse<SubtitleDownloadLink, NetworkError>
+
+    /**
+     * Get the most downloaded subtitles from past 30 days on OpenSubtitles
+     * @see <a href="https://opensubtitles.stoplight.io/docs/opensubtitles-api/open_api.json/paths/~1api~1v1~1discover~1most_downloaded/get">API document</a>
+     * @param languages Optional [String] language codes comma separated like: en,fa or all
+     * @param type Optional [String] , could be one of these values: movie or tvshow
+     * @return [NetworkResponse] including [ApiResponse] that consists of [Subtitle]
+     */
+    @GET("discover/most_downloaded")
+    suspend fun getMostDownloaded(@Query("languages") languages: String? = null, @Query("type") type: String? = null): NetworkResponse<ApiResponse<Subtitle>, NetworkError>
+
+    /**
+     * Lists of 60 latest uploaded subtitles
+     * @see <a href="https://opensubtitles.stoplight.io/docs/opensubtitles-api/open_api.json/paths/~1api~1v1~1discover~1latest/get">API document</a>
+     * @param languages Optional [String] language codes comma separated like: en,fa or all
+     * @param type Optional [String] , could be one of these values: movie or tvshow
+     * @return [NetworkResponse] including [ApiResponse] that consists of [Subtitle]
+     */
+    @GET("discover/latest")
+    suspend fun getLatestSubtitles(@Query("languages") languages: String? = null, @Query("type") type: String? = null): NetworkResponse<ApiResponse<Subtitle>, NetworkError>
 }
