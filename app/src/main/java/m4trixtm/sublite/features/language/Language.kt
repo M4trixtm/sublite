beaff938249e720a4a228b27dbdd9f670c0c63ed
Mohammad Jahangiry <mjahangiry75@gmail.com>
2021-01-31 17:53:53 +0330

    Add Languages service

---
+++ app/src/main/java/m4trixtm/sublite/features/language/Language.kt
@@ -0,0 +1,18 @@
+package m4trixtm.sublite.features.language
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
+data class LanguageResponse(
+    @SerializedName("data")
+    val languages: List<Language>
+)
+
+@Keep
+data class Language(
+    @SerializedName("language_name")
+    val name: String,
+    @SerializedName("language_code")
+    val code: String
+)
---
+++ app/src/test-common/java/m4trixtm/sublite/LanguageMocks.kt
@@ -0,0 +1,26 @@
+package m4trixtm.sublite
+
+import m4trixtm.sublite.features.language.Language
+import m4trixtm.sublite.features.language.LanguageResponse
+
+object LanguageMocks {
+
+    val languageResponse: LanguageResponse
+        get() = LanguageResponse(
+            languages = listOf(
+                Language(
+                    name = "Persian",
+                    code = "fa"
+                ),
+                Language(
+                    name = "Albanian",
+                    code = "sq"
+                ),
+                Language(
+                    name = "Northern Sami",
+                    code = "se"
+                ),
+            )
+        )
+
+}
