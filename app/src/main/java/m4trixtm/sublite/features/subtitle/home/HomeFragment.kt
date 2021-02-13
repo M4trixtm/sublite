5200bdf10cc764281eecf09d05e620916cb80dde
moein <Moeinabdi77@gmail.com>
2021-02-13 16:29:26 +0330

    add home fragment

---
+++ app/src/main/java/m4trixtm/sublite/features/subtitle/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package m4trixtm.sublite.features.subtitle.home
+
+
+import dagger.hilt.android.AndroidEntryPoint
+import m4trixtm.sublite.R
+import m4trixtm.sublite.core.platform.fragment.BaseFragment
+import m4trixtm.sublite.databinding.FragmentHomeBinding
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
