509df358cabefa89c5ef70f9f85d4116d30d390e
moein <Moeinabdi77@gmail.com>
2021-02-20 23:16:39 +0330

    design home fragment. closes #2

--- app/src/main/java/m4trixtm/sublite/features/subtitle/home/HomeFragment.kt
+++ app/src/main/java/m4trixtm/sublite/features/subtitle/home/HomeFragment.kt
@@ -1,12 +1,40 @@
 package m4trixtm.sublite.features.subtitle.home
 
-
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.xwray.groupie.GroupieAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.InternalCoroutinesApi
 import m4trixtm.sublite.R
 import m4trixtm.sublite.core.platform.fragment.BaseFragment
+import m4trixtm.sublite.core.toast.shortToast
 import m4trixtm.sublite.databinding.FragmentHomeBinding
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val homeViewModel: HomeViewModel by viewModels()
 
+    @InternalCoroutinesApi
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            popularAdapter = GroupieAdapter()
+            mostDownloadAdapter = GroupieAdapter()
+            latestAdapter = GroupieAdapter()
+            viewModel = homeViewModel
+        }
+
+        homeViewModel.selectedShow.collectOnLifecycleScope {
+            requireContext().shortToast(it?.title.toString())
+        }
+
+        homeViewModel.selectedSubtitle.collectOnLifecycleScope {
+            requireContext().shortToast(it?.details?.title.toString())
+        }
+
+        homeViewModel.loadHomePage()
+    }
 }
